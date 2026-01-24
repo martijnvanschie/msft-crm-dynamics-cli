3895e65c7eae5c32af9a6ed387b63ea74bd2f3a6
Martijn van Schie <martijnvanschie@gmail.com>
2026-01-24 09:48:42 +0100

    Removed unused code.

--- cli/Microsoft.Dynamics.Cli/MainProcess.cs
+++ cli/Microsoft.Dynamics.Cli/MainProcess.cs
@@ -61,43 +61,6 @@ namespace Microsoft.Dynamics.Cli
                 AnsiConsole.MarkupInterpolated($"[red]{ex.Message}[/]");
                 return -1;
             }
-
-            //HttpClient _http = new HttpClient();
-            //_http.BaseAddress = new Uri("https://api.partnercenter.microsoft.com/v1/");
-            //_http.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", pctoken.AccessToken);
-
-            //var response = await _http.GetAsync("customers");
-            //try
-            //{
-            //	response.EnsureSuccessStatusCode();
-            //}
-            //catch (Exception ex)
-            //{
-            //	Console.WriteLine(ex);
-            //}
-            //var response2 = await response.Content.ReadAsStringAsync();
-
-            ////// Dynamics 365 Sales API call
-            //// Get opportunity by ID
-            ///
-            TokenProvider.clientSecret = CliContext.ClientSecret;
-            TokenProvider.clientId = CliContext.ClientId;
-            TokenProvider.tenantId = CliContext.TenantId;
-
-            string opportunityId = "e59cc412-8adc-483d-8d96-47f4bd0fe4cf"; // Replace with actual opportunity ID
-
-            DynamicsOpportunitiesClient dynamicsOpportunitiesClient = new DynamicsOpportunitiesClient();
-            var opportunities = await dynamicsOpportunitiesClient.GetOpportunitiesWithFields(new string[] { "name", "estimatedvalue", "statuscode" }, 5);
-            Console.WriteLine(opportunities);
-
-            var opportunityById = await dynamicsOpportunitiesClient.GetOpportunity(opportunityId);
-            Console.WriteLine(opportunityById);
-
-            //DynamicsAccountsClient dynamicsAccountsClient = new DynamicsAccountsClient();
-            //var accounts = await dynamicsAccountsClient.GetAccountsByName("Zeeman", 5);
-            //Console.WriteLine(accounts);
-
-            return 0;
         }
     }
 }
